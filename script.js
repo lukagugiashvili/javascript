cc8c1ff7bc4f6058156f64bd96dc3ee16da73bf6
lukagugiashvili <119782216+lukagugiashvili@users.noreply.github.com>
2023-01-16 13:20:53 +0400

    create java

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+let user = "dato";
+let age = 20;
+let isFemale = false;
+let surName = "datoshvili";
+
+console.log(user + " " + surName);
+
+console.log(`${user} ${surName}`);
+
+let x = 10;
+let y = 4;
+
+let sum = x + y;
+let div = x % y;
+let sub = x - y;
+let mul = x * y;
+
+let item = x + div;
+console.log(item);
+
+//martkutxedis fartobi
+let width = 5
+let height = 7
+
+let s = width * height;
+
+console .log(s)
+
+//wris fartpbi
+let p,r;
+p = 3.14
+r = 7
+s = p*r*r
+console.log(s)
+
+
